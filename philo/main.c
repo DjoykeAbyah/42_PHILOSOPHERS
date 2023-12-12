10cbc9b71bf447ad86775b727aa6198a4e26a165
YourGitHubUsername <YourGitHubEmail>
2023-12-12 21:24:02 +0100

    setup framework

---
+++ philo/main.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/12/12 21:00:27 by dreijans      #+#    #+#                 */
+/*   Updated: 2023/12/12 21:23:19 by dreijans      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int main(int argc, char **argv)
+{
+    (void)argv;
+    if (argc != 4 && argc != 5)
+        printf("please give 4 or 5 numeric arguments\n");
+}
---
+++ philo/philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   philo.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/12/12 21:01:11 by dreijans      #+#    #+#                 */
+/*   Updated: 2023/12/12 21:15:59 by dreijans      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+#endif
